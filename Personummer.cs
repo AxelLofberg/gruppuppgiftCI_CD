d743886340fb7931b28197f0b83b7b642ef1a3fa
Sed-93 <sedat-933@hotmail.se>
2024-01-16 19:44:00 +0100

    sed93 first commit

--- Personummer.cs
+++ Personummer.cs
@@ -18,3 +18,23 @@ namespace PersonligVerifiering
                     return false;
                 }
             }
+  // Om personnumret är i formatet "ÅÅMMDDXXXX" eller "ÅÅMMDD-XXXX"
+            if (personnummer.Length == 10  personnummer.Length == 12)
+            {
+                int år = int.Parse(personnummer.Substring(0, 2));
+                int månad = int.Parse(personnummer.Substring(2, 2));
+                int dag = int.Parse(personnummer.Substring(4, 2));
+
+                // Enkel kontroll för år, månad och dag.
+                if (år < 0  månad < 1  månad > 12  dag < 1 || dag > 31)
+                {
+                    return false;
+                }
+            }
+
+            // Ytterligare kontroller för personnummer kan läggas till här.
+
+            return true;
+        }
+    }
+}
